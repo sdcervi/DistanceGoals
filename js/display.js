ba50c52d0204ca44f2a861ad41a5d07d2094b7c9
sdcervi <sdcervi@gmail.com>
2022-03-02 19:44:19 -0800

    Improved design
    - Added more sample data to show different length milestones
    - Moved sample data into its own JS file for better scalability

--- js/display.js
+++ js/display.js
@@ -1,61 +1,4 @@
 const inProgress = document.getElementById("in-progress");
-const test = document.getElementById("test");
-
-const challenges = {
-	zodiac: {
-		name:			"600 Mile Zodiac Challenge",
-		company:		"Medal Chasers",
-		distance:		600,
-		unit:			"miles",
-		period:			365,
-		start:			"2-28-2022",
-		progress:		0,
-		complete:		false,	
-		milestones:	[
-			{	name:	"Capricorn",	distance:	50		},
-			{	name:	"Aquarius",		distance:	100		},
-			{	name:	"Pisces",		distance:	150		},
-			{	name:	"Aries",		distance:	200		},
-			{	name:	"Taurus",		distance:	250		},
-			{	name:	"Gemini",		distance:	300		},
-			{	name:	"Cancer",		distance:	350		},
-			{	name:	"Leo",			distance:	400		},
-			{	name:	"Virgo",		distance:	450		},
-			{	name:	"Libra",		distance:	500		},
-			{	name:	"Scorpio",		distance:	550		},
-			{	name:	"Saggitarius",	distance:	600		}]
-	},
-	rise_and_run: {
-		name:			"Rise &amp; Run",
-		company:		"Medal Chasers",
-		distance:		5,
-		unit:			"km",
-		period:			30,
-		start:			"3-1-2022",
-		progress:		0,
-		complete:		false
-	},
-	dragon: {
-		name:			"Unleash the Dragon Within",
-		company:		"Medal Chasers",
-		distance:		5,
-		unit:			"km",
-		period:			30,
-		start:			"3-1-2022",
-		progress:		0,
-		complete:		false
-	},
-	ring: {
-		name:			"One Run to Rule Them All",
-		company:		"Medal Chasers",
-		distance:		5,
-		unit:			"km",
-		period:			30,
-		start:			"3-1-2022",
-		progress:		0,
-		complete:		false
-	}
-};
 
 function writeCard () {
 	for (const counter in challenges) {
@@ -73,7 +16,9 @@ function writeCard () {
 			let cardContent = '<article class="col"><div class="card h-100 race-card">';
 			cardContent += '<div class="card-header race-name"><h3>' + challenge.name + '</h3></div>';
 			cardContent += '<div class="card-body">';
-			cardContent += '<p class="race-period">Finishes: ' + endDate.toDateString() + '</p>';
+			if (challenge.period) {
+				cardContent += '<p class="race-period">Finishes: ' + endDate.toDateString() + '</p>';
+			}
 			cardContent += '<h4 class="card-title race-company">' + challenge.company + '</h4>';
 			cardContent += '<div class="race-progress"><div class="row"><div class="col-3 progress-header">Distance</div><div class="col-9 progress-display"><div class="progress">';
 			if (challenge.milestones) {
@@ -104,13 +49,15 @@ function writeCard () {
 			} else {
 				cardContent += '<div class="progress-bar" role="progressbar" style="width: ' + progress + '%;" aria-valuenow="' + progress + '" aria-valuemin="0" aria-valuemax="100">' + progress + '%</div>'
 			}
-			cardContent += '</div></div></div><div class="row"><div class="col-3 progress-header">Time</div><div class="col-9 progress-display"><div class="progress">';
-			cardContent += '<div class="progress-bar" role="progressbar" style="width: ' + (timeSoFar / challenge.period) * 100 + '%;" aria-valuenow="' + (timeSoFar / challenge.period) * 100 + '" aria-valuemin="0" aria-valuemax="100"></div></div></div></div></div>';
+			if (challenge.period) {
+				cardContent += '</div></div></div><div class="row"><div class="col-3 progress-header">Time</div><div class="col-9 progress-display"><div class="progress">';
+				cardContent += '<div class="progress-bar" role="progressbar" style="width: ' + (timeSoFar / challenge.period) * 100 + '%;" aria-valuenow="' + (timeSoFar / challenge.period) * 100 + '" aria-valuemin="0" aria-valuemax="100"></div>';
+			}
+			cardContent += '</div></div></div></div>';
 			cardContent += '<div class="race-details"><p><strong>Goal:</strong> ' + challenge.distance + " " + challenge.unit + '</p><p><strong>Progress:</strong> ' + challenge.progress + " " + challenge.unit + '</p>';
 			if (challenge.milestones) {
 				cardContent += '<p><strong>Milestones:</strong></p></div><div class="milestone-progress">';
 				let stepCounter = 0;
-				let stepSize = 0;
 				let previousStep = 0;
 				let currentStep = 0;
 				for (const step in challenge.milestones) {
@@ -126,7 +73,8 @@ function writeCard () {
 						currentStep = Math.max(challenge.progress - previousStep, 0);
 						let nextStep = challenge.milestones[stepCounter].distance - previousStep;
 						cardContent += '<div class="row"><div class="col-3 progress-header">' + milestone.name + '</div><div class="col-9 progress-display">';
-						cardContent += '<div class="progress"><div class="progress-bar" role="progressbar" style="width: ' + (currentStep / nextStep) * 100 + '%;" aria-valuenow="' + nextStep + '" aria-valuemin="0" aria-valuemax="100">' + Math.floor((currentStep / nextStep) * 100) + '%</div></div>';
+						//cardContent += '<div class="progress"><div class="progress-bar" role="progressbar" style="width: ' + (currentStep / nextStep) * 100 + '%;" aria-valuenow="' + nextStep + '" aria-valuemin="0" aria-valuemax="100">' + Math.floor((currentStep / nextStep) * 100) + '%</div></div>';
+						cardContent += '<div class="progress"><div class="progress-bar" role="progressbar" style="width: ' + (currentStep / nextStep) * 100 + '%;" aria-valuenow="' + nextStep + '" aria-valuemin="0" aria-valuemax="100">' + currentStep.toFixed(2) + " / " + nextStep + '</div></div>';
 						cardContent += '</div></div>';
 						previousStep = challenge.milestones[stepCounter].distance;
 						//break;
---
+++ js/sample-data.js
@@ -0,0 +1,74 @@
+const challenges = {
+	zodiac: {
+		name:			"600 Mile Zodiac Challenge",
+		company:		"Medal Chasers",
+		distance:		600,
+		unit:			"miles",
+		period:			365,
+		start:			"2-28-2022",
+		progress:		384.39,
+		complete:		false,	
+		milestones:	[
+			{	name:	"Capricorn",	distance:	50		},
+			{	name:	"Aquarius",		distance:	100		},
+			{	name:	"Pisces",		distance:	150		},
+			{	name:	"Aries",		distance:	200		},
+			{	name:	"Taurus",		distance:	250		},
+			{	name:	"Gemini",		distance:	300		},
+			{	name:	"Cancer",		distance:	350		},
+			{	name:	"Leo",			distance:	400		},
+			{	name:	"Virgo",		distance:	450		},
+			{	name:	"Libra",		distance:	500		},
+			{	name:	"Scorpio",		distance:	550		},
+			{	name:	"Saggitarius",	distance:	600		}]
+	},
+	rise_and_run: {
+		name:			"Rise &amp; Run",
+		company:		"Medal Chasers",
+		distance:		5,
+		unit:			"km",
+		period:			false,
+		start:			"3-1-2022",
+		progress:		2.53,
+		complete:		false
+	},
+	dragon: {
+		name:			"Unleash the Dragon Within",
+		company:		"Medal Chasers",
+		distance:		5,
+		unit:			"km",
+		period:			false,
+		start:			"3-1-2022",
+		progress:		3.46,
+		complete:		false
+	},
+	ring: {
+		name:			"One Run to Rule Them All",
+		company:		"Medal Chasers",
+		distance:		5,
+		unit:			"km",
+		period:			false,
+		start:			"3-1-2022",
+		progress:		4.70,
+		complete:		false
+	},
+	space_race: {
+		name:			"Space Race",
+		company:		"Virtual Running Club",
+		distance:		400,
+		unit:			"miles",
+		period:			false,
+		start:			"2-3-2022",
+		progress:		101.81,
+		complete:		false,	
+		milestones:	[
+			{	name:	"Mercury",	distance:	25		},
+			{	name:	"Venus",	distance:	70		},
+			{	name:	"Earth",	distance:	120		},
+			{	name:	"Mars",		distance:	170		},
+			{	name:	"Jupiter",	distance:	225		},
+			{	name:	"Saturn",	distance:	280		},
+			{	name:	"Uranus",	distance:	340		},
+			{	name:	"Neptune",	distance:	400		}]
+	}
+};
