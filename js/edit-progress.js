7cfe7a04d63ec82e142a4622a3bd36ff74df1b7b
sdcervi <sdcervi@gmail.com>
2022-03-04 14:45:51 -0800

    Added add/edit capabilities to increment or edit total

--- js/edit-progress.js
+++ js/edit-progress.js
@@ -1,9 +1,18 @@
 const editProgressModal = document.getElementById('editProgressModal');
+const addProgressModal = document.getElementById('addProgressModal');
 let challengeID = "";
+
 editProgressModal.addEventListener('show.bs.modal', function (event) {
 	const button = event.relatedTarget;
-	challengeID = button.getAttribute('data-bs-challenge')
-	const modalBodyInput = editProgressModal.querySelector('.modal-body #challenge-name');
+	challengeID = button.getAttribute('data-bs-challenge');
+	const modalBodyInput = editProgressModal.querySelector('.modal-body #challenge-edit-name');
+	modalBodyInput.value = challenges[challengeID].name;
+});
+
+addProgressModal.addEventListener('show.bs.modal', function (event) {
+	const button = event.relatedTarget;
+	challengeID = button.getAttribute('data-bs-challenge');
+	const modalBodyInput = addProgressModal.querySelector('.modal-body #challenge-add-name');
 	modalBodyInput.value = challenges[challengeID].name;
 });
 
@@ -24,6 +33,32 @@ function editProgress () {
 		challenge.progress = distance * 1.60934;
 	} else if (selectedUnit == "kilometers" && challenge.unit == "miles") {
 		challenge.progress = distance * 0.621371;
+	} else {
+		alert ("Unit conversion error.");
+	}
+	inProgress.innerHTML = "";
+	complete.innerHTML = "";
+	writeCard();
+	saveChanges();
+}
+
+function addProgress () {
+	const distance = parseFloat(document.getElementById('enter-distance-add').value);
+	const units = document.querySelectorAll('input[name="distance-add"]');
+	const challenge = challenges[challengeID];
+	let selectedUnit;
+	for (const unit of units) {
+		if (unit.checked) {
+			selectedUnit = unit.value;
+			break;
+		}
+	}
+	if (selectedUnit == challenge.unit) {
+		challenge.progress += distance;
+	} else if (selectedUnit == "miles" && challenge.unit == "kilometers") {
+		challenge.progress += distance * 1.60934;
+	} else if (selectedUnit == "kilometers" && challenge.unit == "miles") {
+		challenge.progress += distance * 0.621371;
 	} else {
 		alert ("Unit conversion error.");
 	}
